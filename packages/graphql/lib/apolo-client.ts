8cd1d0b0d86b18484eb15b6832698e84f10c9e8d
RAMASSAMY Luc <luko974@gmail.com>
2025-05-22 10:26:50 +0200

    POC - graphql subs

---
+++ packages/graphql/lib/apolo-client.ts
@@ -0,0 +1,39 @@
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { createClient } from 'graphql-ws'
+
+const httpLink = new HttpLink({
+  uri: 'https://dev.base.intuition-api.com/v1/graphql', // replace with your API URL
+})
+
+const wsLink =
+  typeof window !== 'undefined'
+    ? new GraphQLWsLink(
+        createClient({ url: 'wss://dev.base.intuition-api.com/v1/graphql' }),
+      ) // replace with your WS endpoint
+    : null
+
+const splitLink =
+  typeof window !== 'undefined' && wsLink
+    ? split(
+        ({ query }) => {
+          const def = getMainDefinition(query)
+
+          return (
+            def.kind === 'OperationDefinition' &&
+            def.operation === 'subscription'
+          )
+        },
+
+        wsLink,
+
+        httpLink,
+      )
+    : httpLink
+
+export const apolloClient = new ApolloClient({
+  link: splitLink,
+
+  cache: new InMemoryCache(),
+})
