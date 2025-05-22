cc7e2a6689e56b80b93e4b3a98de00640cc0e1a9
RAMASSAMY Luc <luko974@gmail.com>
2025-05-22 17:10:53 +0200

    subscriptions hook in index.ts

--- packages/graphql/codegen.ts
+++ packages/graphql/codegen.ts
@@ -3,36 +3,6 @@ import type { Types } from '@graphql-codegen/plugin-helpers'
 
 import { API_URL_PROD } from './src/constants'
 
-const commonGenerateOptions: Types.ConfiguredOutput = {
-  config: {
-    reactQueryVersion: 5,
-    fetcher: {
-      func: '../client#fetcher',
-      isReactHook: false,
-    },
-    exposeDocument: true,
-    exposeFetcher: true,
-    exposeQueryKeys: true,
-    exposeMutationKeys: true,
-    addInfiniteQuery: true,
-    enumsAsTypes: true,
-    dedupeFragments: true,
-    documentMode: 'documentNode',
-    scalars: {
-      Date: 'Date',
-      JSON: 'Record<string, any>',
-      ID: 'string',
-      Void: 'void',
-    },
-  },
-  plugins: [
-    'typescript',
-    '@graphql-codegen/typescript-operations',
-    '@graphql-codegen/typescript-react-query',
-    'typescript-document-nodes',
-  ],
-}
-
 const config: CodegenConfig = {
   overwrite: true,
   hooks: { afterAllFileWrite: ['prettier --write'] },
@@ -47,23 +17,31 @@ const config: CodegenConfig = {
   ignoreNoDocuments: true,
   documents: ['**/*.graphql'],
   generates: {
-    // Main output using react-query
     './src/generated/index.ts': {
-      config: {
-        ...commonGenerateOptions.config,
-      },
-      plugins: commonGenerateOptions.plugins,
-    },
-
-    // Apollo Client output for subscriptions
-    './src/generated/subscriptions.ts': {
-      documents: ['src/graphql/subscriptions/**/*.graphql'], // Or wherever you place your .graphql subscription ops
+      documents: [
+        'src/graphql/**/*.graphql', // Inclut tout, y compris les subscriptions
+      ],
       plugins: [
         'typescript',
         '@graphql-codegen/typescript-operations',
+        '@graphql-codegen/typescript-react-query',
         '@graphql-codegen/typescript-react-apollo',
+        'typescript-document-nodes',
       ],
       config: {
+        reactQueryVersion: 5,
+        fetcher: {
+          func: '../client#fetcher',
+          isReactHook: false,
+        },
+        exposeDocument: true,
+        exposeFetcher: true,
+        exposeQueryKeys: true,
+        exposeMutationKeys: true,
+        addInfiniteQuery: true,
+        enumsAsTypes: true,
+        dedupeFragments: true,
+        documentMode: 'documentNode',
         withHooks: true,
         withHOC: false,
         withComponent: false,
@@ -73,11 +51,9 @@ const config: CodegenConfig = {
           ID: 'string',
           Void: 'void',
         },
-        documentMode: 'documentNode',
       },
     },
 
-    // Optional: export your schema
     './schema.graphql': {
       plugins: ['schema-ast'],
       config: {
